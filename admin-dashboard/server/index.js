b7636f7962259550e0279a881f3419f193faf231
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-21 23:15:32 -0500

    base folder (server)

--- admin-dashboard/server/index.js
+++ admin-dashboard/server/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import core from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
