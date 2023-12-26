1702d4a160d6f783924a0a0c961ff0f73405b825
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-25 19:31:00 -0500

    customers page, api, dataGrid

--- admin-dashboard/client/src/App.js
+++ admin-dashboard/client/src/App.js
@@ -8,6 +8,7 @@ import { themeSettings } from "theme";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
 import Products from "scenes/products";
+import Customers from "scenes/customers";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -27,6 +28,7 @@ function App() {
                             />
                             <Route path="/dashboard" element={<Dashboard />} />
                             <Route path="/products" element={<Products />} />
+                            <Route path="/customers" element={<Customers />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>
